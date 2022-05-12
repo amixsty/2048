7d7bfb1d28d993920ef2b7f163156fa1592aa078
Amir Hossein Fattahi <amixsty@gmail.com>
2022-05-12 11:45:51 +0430

    wow! now we can move UP / RIGHT / LEFT / DOWN...

--- form1.cs
+++ form1.cs
@@ -8,11 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace _2048
+namespace _2048game
 {
     public partial class Form1 : Form
     {
         Label[,] game_board;
+        int n = 4;
         public Form1()
         {
             InitializeComponent();
@@ -23,16 +24,16 @@ namespace _2048
                 {
                     game_board[i, j] = new Label();
 
-                    game_board[i, j].Text = "2";
+                    //  game_board[i, j].Text = "2";
 
-                    game_board[i, j].Anchor = AnchorStyles.Top | 
+                    game_board[i, j].Anchor = AnchorStyles.Top |
                                               AnchorStyles.Bottom |
-                                                     AnchorStyles.Left | 
+                                                     AnchorStyles.Left |
                                                          AnchorStyles.Right;
 
                     game_board[i, j].Font = new Font("Tahoma", 33);
-
-                    game_board[i, j].BackColor = Color.Brown;
+                    game_board[i, j].ForeColor = Color.FromArgb(118, 112, 104);
+                    game_board[i, j].BackColor = Color.FromArgb(205,193,181);
 
                     game_board[i, j].BorderStyle = new BorderStyle();
 
@@ -49,31 +50,110 @@ namespace _2048
                 }
 
             }
-        }
+            Random r = new Random();
+            int r1_x, r2_x, r1_y, r2_y, random_index, r1, r2;
+            int[] init_numbers = { 2, 2, 2, 2, 4 };
 
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyData == Keys.Left)
+            random_index = r.Next(0, init_numbers.Length);
+            r1 = init_numbers[random_index];
+
+            random_index = r.Next(0, init_numbers.Length);
+            r2 = init_numbers[random_index];
+
+
+            r1_x = r.Next(0, 3);
+            r1_y = r.Next(0, 3);
+
+            do
             {
-                MessageBox.Show("چپ");
-            }
-            else if (e.KeyData == Keys.Right)
-            {
-                MessageBox.Show("راست");
-            }
-            else if (e.KeyData == Keys.Up)
-            {
-                MessageBox.Show("بالا");
-            }
-            else if (e.KeyData == Keys.Down)
-            {
-                MessageBox.Show("پایین");
-            }
+                r2_x = r.Next(0, 3);
+                r2_y = r.Next(0, 3);
+            } while (r1_x == r2_x && r1_y == r2_y);
+
+            game_board[r1_x, r1_y].Text = Convert.ToString(r1);
+            game_board[r2_x, r2_y].Text = Convert.ToString(r2);
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (i > 0 && game_board[i - 1, j].Text == "")
+                            {
+                                game_board[i - 1, j].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+
+                            }
+                        }
+                    }
+                }
+            }
+            else if (e.KeyData == Keys.Right)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (i < n - 1 && game_board[i + 1, j].Text == "")
+                            {
+                                game_board[i + 1, j].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+
+                            }
+                        }
+                    }
+                }
+            }
+            else if (e.KeyData == Keys.Down)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (j < n - 1 && game_board[i, j + 1].Text == "")
+                            {
+                                game_board[i, j + 1].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+                            }
+                        }
+                    }
+                }
+            }
+            else if (e.KeyData == Keys.Up)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (j > 0 && game_board[i, j - 1].Text == "")
+                            {
+                                game_board[i, j - 1].Text = game_board[i, j].Text;
+                                game_board[i, j].Text = "";
+
+                            }
+                        }
+                    }
+                }
+            }
+        
+    }
     }
 }
